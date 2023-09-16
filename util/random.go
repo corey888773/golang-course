da9c1887215681f05ad431dde883f40dd6bd0f6c
Piotr Gąsiorek <piotrgasiorek773@gmail.com>
2023-09-16 21:45:14 +0200

    initial commit

---
+++ util/random.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var stringBuilder strings.Builder
+	alphabetLength := len(alphabet)
+	for i := 0; i < n; i++ {
+		char := alphabet[rand.Intn(alphabetLength)]
+		stringBuilder.WriteByte(char)
+	}
+	return stringBuilder.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
